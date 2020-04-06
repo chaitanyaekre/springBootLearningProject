0125d2157be20fdf0cab96f8db3e6a2a464903e4
Chaitanya <chaitanya.e@toranainc.com>
2020-04-06 16:01:46 +0530

    Commiting project for first time

---
+++ src/main/java/com/vega/springit/domain/Comment.java
@@ -0,0 +1,23 @@
+package com.vega.springit.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@Data
+public class Comment extends Auditable{
+
+	@Id
+	@GeneratedValue
+	private long id;
+	private String body;
+	
+	@ManyToOne
+	private Link link;
+}
