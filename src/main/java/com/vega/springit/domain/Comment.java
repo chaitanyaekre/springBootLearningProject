4563b15fd095646f32c53017b25fc1ee3aca7862
Chaitanya <chaitanya.e@toranainc.com>
2020-04-07 15:00:02 +0530

    added database layer to the application

--- src/main/java/com/vega/springit/domain/Comment.java
+++ src/main/java/com/vega/springit/domain/Comment.java
@@ -6,18 +6,28 @@ import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @NoArgsConstructor
-@Data
+@Getter @Setter
+@ToString
+@RequiredArgsConstructor
 public class Comment extends Auditable{
 
 	@Id
 	@GeneratedValue
 	private long id;
+	@NonNull
 	private String body;
 	
 	@ManyToOne
+	@NonNull
 	private Link link;
+
 }
